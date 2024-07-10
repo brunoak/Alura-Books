6976134ffaf24d1242a232c5f8d2d476f0e181ae
Bruno Carvalho <bruno.oak23@gmail.com>
2024-07-10 18:08:32 -0300

    refactor: refactoring the index and main

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 let livros = [];
+let livrosComDesconto = [];
 const endpointDaApi = `https://guilhermeonrails.github.io/casadocodigo/livros.json`
 getBuscaLivrosDaAPI();
 
 async function getBuscaLivrosDaAPI(){
     const res = await fetch(endpointDaApi)
     livros = await res.json()
-    let livrosComDesconto = aplicarDesconto(livros);
+    livrosComDesconto = aplicarDesconto(livros);
     //aplicaFilter(livros);
     
     exibirLivros(livrosComDesconto);
