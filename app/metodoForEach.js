3f80610c73abc48fee0005b653c853d98dce9a1f
Bruno Carvalho <bruno.oak23@gmail.com>
2024-07-10 18:11:33 -0300

    create: showing the datas on the screen

---
+++ app/metodoForEach.js
@@ -0,0 +1,21 @@
+let elementoParaInserirLivros = document.querySelector('#livros');
+
+function exibirLivros(listaDeLivros){
+    elementoParaInserirLivros.innerHTML='';
+    listaDeLivros.forEach(livro => {
+        let disponivel = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel" 
+        elementoParaInserirLivros.innerHTML +=`
+            <div class="livro">
+                <img class="${disponivel}" src=${livro.imagem} alt=${livro.alt}/>
+                <h2 class="livro__titulo">
+                    ${livro.titulo}
+                </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+                 <div class="tags">
+                    <span class="tag">${livro.categoria}</span>
+                </div>
+            </div>
+        `
+    });
+}
